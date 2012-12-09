5243274f73dcfaf44aa01df0bb01bc98ea63f069
Alfonso Saavedra Son <sonlink.dourden@gmail.com>
2012-12-09 15:32:08 +0100

    r4: Ahora puede usarse como galeria unica o multi galeria

--- galeria.php
+++ galeria.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
-		<title>Galeria en subcarpetas</title>
+		<meta charset="utf-8">
+		<title>Galería en subcarpetas</title>
 		<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
 		<script src="js/jquery-1.7.2.min.js"></script>
 		<script src="js/lightbox.js"></script>
@@ -13,78 +14,103 @@
 
 # Configuración del script
 # Directorio donde están las imágenes. NO usar rutas relativas (p.e: ../galeria o ./galeria)
-ini_set("display_errors", 1);
+#ini_set("display_errors", 1);
 $path = 'fotos';
+$multi = false; #Poner a true si quieres usar subcarpetas
 $limit = 20; # Cuantas imáenes se mostraran por pagina
 $limit_file = 5; # Imágenes a mostrar por linea en la tabla
-$n = 0;
-$desde;
-$hasta;
-$list = array();
-# Comprobamos si es un directorio y si lo es nos movemos a el
-if (is_dir($path)){
-	if (!isset($_GET['gal'])){
-		$folders = glob("$path/*", GLOB_ONLYDIR);
-		if ($folders){
-			foreach($folders as $folder) {
-				print $folder;
-				$folder = preg_replace("/$path\//" ,'', $folder);
-				print "<a href=\"?gal=$folder\">" . $folder . "</a><br />\n";
-			}
-		}else{
-			print "$path no contiene ninguna subcarpeta";
-		}
-	}else{
-		$dir = $path.'/'.$_GET['gal'];
-		# Recorremos los ficheros que hay en el directorio y cogemos solamente aquellos cuya extensión
-		# sea jpg, gif y png y la guardamos en una lista
-		foreach(glob("$dir/*.*" , GLOB_BRACE ) as $file) {
-			if (preg_match("#([\w\s]+)\.(gif|GIF|jpg|JPG|png|PNG)#is",$file)){
-				$list[] = $file;
+
+function view_gallery($folder){
+	global $limit, $limit_file, $path, $multi;
+	$desde;
+	$hasta;
+	$list = Array();
+	# Recorremos los ficheros que hay en el directorio y cogemos solamente aquellos cuya extensión
+	# sea jpg, gif y png y la guardamos en una lista
+	$images = glob("$path/*",  GLOB_BRACE);
+	if ($images){
+		foreach($images as $image) {
+			if (preg_match("#([\w\s]+)\.(gif|GIF|jpg|JPG|png|PNG)#is",$image)){
+				$list[] = $image;
 			}
 		}
 
 		# Contamos el total de elementos en la lista
 		$total = count($list);
-		$paginas = ceil($total/$limit);
-		if (!isset($_GET['pg'])){
-			$desde = 0;
-			$hasta = $desde + $limit;
-		}else if((int)$_GET['pg'] > ($paginas-1)){
-			# Si pg es mayor que el total de paginas se muestra un error
-			print "<b>No existe esta pagina en la galería</b>
-			<a href='index.php'>Volver a la galería</a>";
-			die();
-		}else{
-		$desde = (int)$_GET['pg'];
-		}
-		# Y generamos los enlaces con los thumbnails
-		print "<table>\n<tr>";
-		$n = 0;
-		for ($i=($desde*$limit);($i<=$total) && ($i<($desde*$limit)+$limit);$i++){
-		# Comprobamos si existe en la lista una llave con el valor actual de $i para evitar errores
-			if(array_key_exists($i, $list)){
-				print "<td><a href='$list[$i]' ><img src='thumb.php?img=$list[$i]' /></a>
-				</td>\n";
-				$n++;
-				if ($n == $limit_file){
-					print "</tr>\n<tr>\n";
-					$n = 0;
+		if ($total > 0){
+			$paginas = ceil($total/$limit);
+			if (!isset($_GET['pg'])){
+				$desde = 0;
+				$hasta = $desde + $limit;
+			}else if((int)$_GET['pg'] > ($paginas-1)){
+				# Si pg es mayor que el total de paginas se muestra un error
+				print "<b>No existe esta pagina en la galería</b>
+				<a href='index.php'>Volver a la galería</a>";
+				die();
+			}else{
+				$desde = (int)$_GET['pg'];
+			}
+			# Y generamos los enlaces con los thumbnails
+			print "<table>\n<tr>";
+			$n = 0;
+			for ($i=($desde*$limit);($i<=$total) && ($i<($desde*$limit)+$limit);$i++){
+			# Comprobamos si existe en la lista una llave con el valor actual de $i para evitar errores
+				if(array_key_exists($i, $list)){
+					print "<td><a href='$list[$i]' ><img src='thumb.php?img=$list[$i]' /></a>
+					</td>\n";
+					$n++;
+					if ($n == $limit_file){
+						print "</tr>\n<tr>\n";
+						$n = 0;
+					}
 				}
 			}
-		}
-		print "</tr>\n</table>\n<p id=\"paginas\">";
+			print "</tr>\n</table>\n<p id=\"paginas\">";
 
-		# Generamos un listado de las paginas de la galería
-		for ($p = 0; $p<$paginas; $p++){
-			$pg = $p+1;
-			if ($p == $desde){
-			print "$pg ";
+			# Generamos un listado de las paginas de la galería
+			for ($p = 0; $p<$paginas; $p++){
+				$pg = $p+1;
+				if ($p == $desde){
+					print "$pg ";
+				}else{
+					if ($multi == true){
+						print "<a href ='?gal={$_GET['gal']}&pg=$p'>$pg</a> ";
+					}else{
+						print "<a href ='?pg=$p'>$pg</a> ";
+					}
+				}
+			}
+			print "</p>\nHay un total de $total imagen(es) en $paginas paginas(s)";
 		}else{
-			print "<a href ='?gal={$_GET['gal']}&pg=$p'>$pg</a> ";
+			print "No hay ninguna imagen en $path, están dañadas o no están soportadas";
 		}
+	}else{
+		print "No hay ninguna imagen en $path, están dañadas o no están soportadas";
 	}
-	print "</p>\nHay un total de $total imagen(es) en $paginas paginas(s)";
+}
+
+if (is_dir($path)){
+	if ($multi == false){
+		if (!isset($_GET['gal'])){
+			view_gallery($path);
+		}else{
+			print 'Esta galería no existe';
+		}
+	}else if ($multi == true){
+		if (!isset($_GET['gal'])){
+			$folders = glob("$path/*", GLOB_ONLYDIR);
+			if ($folders){
+				foreach($folders as $folder) {
+					$folder = preg_replace("/$path\//" ,'', $folder);
+					print "<a href=\"?gal=$folder\">" . $folder . "</a><br />\n";
+				}
+			}else{
+				print "$path no contiene ninguna subcarpeta";
+			}
+		}else{
+			$dir = $path.'/'.$_GET['gal'];
+			view_gallery($dir);
+		}
 	}
 }else{
 	print "$path no es un directorio";
